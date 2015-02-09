a1d49b2eef15bf611b323530cb33d6e84bcb3b79
villendu <villendu@buffalo.edu>
2015-02-08 20:40:13 -0500

    Second Commit

---
+++ src/cse/buffalo/edu/cse562/Demo.java
@@ -0,0 +1,10 @@
+package cse.buffalo.edu.cse562;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+	}
+
+}
