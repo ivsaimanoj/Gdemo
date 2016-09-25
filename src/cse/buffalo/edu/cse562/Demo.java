167f3e2dd478833dc97ad81c3038c2dc451b70f8
villendu <villendu@buffalo.edu>
2016-09-24 22:53:54 -0500

    test commit

--- src/cse/buffalo/edu/cse562/Demo.java
+++ src/cse/buffalo/edu/cse562/Demo.java
@@ -5,6 +5,7 @@ public class Demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world2");
+		System.out.print("Testing on 09/24/2016 for knowing the working of intelliJ studio.");
 	}
 
 }
