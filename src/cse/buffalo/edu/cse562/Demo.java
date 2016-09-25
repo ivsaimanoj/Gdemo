9c2f210515ea0696e3552056fb2a691409cab652
villendu <villendu@buffalo.edu>
2016-09-25 02:10:07 -0500

    learning IntelliJ studio ultimate

--- src/cse/buffalo/edu/cse562/Demo.java
+++ src/cse/buffalo/edu/cse562/Demo.java
@@ -1,11 +1,29 @@
 package cse.buffalo.edu.cse562;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class Demo {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
+		int valManoj = 587;
+		String regexx = "^4[0-9]";
+		BufferedReader br = new BufferedReader(new FileReader("abc.txt"));
 		System.out.println("Hello world2");
-		System.out.print("Testing on 09/24/2016 for knowing the working of intelliJ studio.");
-	}
+		System.out.print("Testing on 09/24/2016 for knowing the working of intelliJ studio");
+		Person person = new Person();
+		if (person != null) {
+			System.out.print("person is not null");
+		}
+
+		if (regexx != null) {
+
+		}
+}
+}
+
+class Person {
 
 }
